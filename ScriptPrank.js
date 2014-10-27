b7598b84397cc772a2ee8992ab7e218a20aad37d
Azura <azuradev@gmail.com>
2014-10-27 19:58:42 -0400

    Update ScriptPrank.js

--- ScriptPrank.js
+++ ScriptPrank.js
@@ -1,6 +1,9 @@
 javascript:
 var prompt1 = 'Input Link Here To Prank Your Friend';
+var prompt2 = 'Input Text Here For The Alert Spammer';
 var x = 0;
+var y = 0;
+var webPrankAlert;
 var webPrank;
 document.onkeydown = prankScript;
 function prankScript(keyBoard) {
@@ -12,7 +15,13 @@ function prankScript(keyBoard) {
   }
   if(keyBoard.keyCode == 50){
     webPrank = prompt(prompt1);
+    webPrankAlert = prompt(prompt2);
   }else if(keyBoard.keyCode == 51){
     x = 0;
+  }else if(keyBoard.keyCode == 52){
+      y = 1;
+      while(y == 1){
+          alert(webPrankAlert);
+      }
   }
 }
