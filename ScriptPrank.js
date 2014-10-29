feec78e35e928a46ec53586fce64198b45546c48
Azura <azuradev@gmail.com>
2014-10-28 21:16:22 -0400

    Update ScriptPrank.js

--- ScriptPrank.js
+++ ScriptPrank.js
@@ -11,30 +11,38 @@ var webPrankPass;
 document.onkeydown = prankScript;
 
 function prankScript(keyBoard) {
-    if (keyBoard.keyCode == 49) {
+    if (keyBoard.keyCode === 49) {
         x = 1;
-        while (x == 1) {
+        while (x === 1) {
             window.open(webPrank);
         }
     }
-    if (keyBoard.keyCode == 50) {
-        webPrank = prompt(prompt1);
-        webPrankAlert = prompt(prompt2);
-        webPrankPass = prompt(prompt3);
-    } else if (keyBoard.keyCode == 51) {
+    if (keyBoard.keyCode === 50) {
+        z += 1;
+        if (z === 1) {
+            webPrank = prompt(prompt1);
+        } else if (z === 2) {
+            webPrankAlert = prompt(prompt2);
+        } else if (z === 3) {
+            webPrankPass = prompt(prompt3);
+        }
+    } else if (keyBoard.keyCode === 51) {
         x = 0;
         y = 0;
         window.location.reload(true);
-    } else if (keyBoard.keyCode == 52) {
+    } else if (keyBoard.keyCode === 52) {
         y = 1;
-        while (y == 1) {
+        while (y === 1) {
             alert(webPrankAlert);
         }
     }
-    if(keyBoard.keyCode == 53){
-     var prankPass = webPrankPass;
+    if (keyBoard.keyCode === 53) {
+        var prankPass = webPrankPass;
         var prankPassArea = prompt(prompt3);
-        if(prankPass = prankPassArea)
+        if (prankPass = prankPassArea)
             alert('great');
         else {
             window.location = google;
+        }
+    }
+}
